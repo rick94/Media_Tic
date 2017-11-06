0aed8337123eb1955043b264b682a3008bb52255
rick94 <rick94sm@gmail.com>
2017-11-05 22:09:29 -0600

    Quitar caracteres y primeras pruebas.
    También se ejecutaron

--- fb_scrape_public.py
+++ fb_scrape_public.py
@@ -1,32 +1,8 @@
 from Neo4JQueryBuilder import *
-import copy
-import csv
 import json
 import time
 import urllib.request
-
-def load_data(data, enc='utf-8'):
-    if type(data) is str:
-        csv_data = []
-        with open(data, 'r', encoding=enc, errors='replace') as f:
-            reader = csv.reader((line.replace('\0', '') for line in f))  # remove NULL bytes
-            for row in reader:
-                if row != []:
-                    csv_data.append(row)
-        return csv_data
-    else:
-        return copy.deepcopy(data)
-
-
-def save_csv(filename, data, use_quotes=True, file_mode='w',
-             enc='utf-8'):  # this assumes a list of lists wherein the second-level list items contain no commas
-    with open(filename, file_mode, encoding=enc) as out:
-        for line in data:
-            if use_quotes == True:
-                row = '"' + '","'.join([str(i).replace('"', "'") for i in line]) + '"' + "\n"
-            else:
-                row = ','.join([str(i) for i in line]) + "\n"
-            out.write(row)
+import re
 
 
 def url_retry(url):
@@ -44,19 +20,9 @@ def url_retry(url):
     return json_out
 
 
-def optional_field(dict_item, dict_key):
-    try:
-        out = dict_item[dict_key]
-        if dict_key == 'shares':
-            out = dict_item[dict_key]['count']
-        if dict_key == 'likes':
-            out = dict_item[dict_key]['summary']['total_count']
-    except KeyError:
-        out = ''
-    return out
+#Métodos propios-----------------------------------------------------------------------
 
 
-#Métodos propios------------------------------------------------------------------------
 
 def getAccessToken(client_id, client_secret):
     fb_urlobj = urllib.request.urlopen('https://graph.facebook.com/oauth/access_token?grant_type=client_credentials&client_id=' + client_id + '&client_secret=' + client_secret)
@@ -65,7 +31,7 @@ def getAccessToken(client_id, client_secret):
 
 def getReactionRelatedData(reaction, reacted_object_id, reacted_object_label):
     queryList = []
-    attributeList = [('name', reaction['name'])]
+    attributeList = [('name', re.sub("[*@#'$]", '',reaction['name']))]
     user_node_insertion_query = buildInsertOrUpdateNodeQuery('User', reaction['id'], attributeList)
     attributeList = [('type', reaction['type'])]
     reaction_user_relationship_query = buildInsertOrUpdateRelationshipQuery('REACTS_TO', 'User', reaction['id'], \
@@ -75,12 +41,12 @@ def getReactionRelatedData(reaction, reacted_object_id, reacted_object_label):
 
 def getReplyRelatedData(reply, comment_id, post_id):
     queryList = []
-    attributeList = [('date', reply['created_time'])]
+    attributeList = [('date', reply['created_time']),('text', re.sub("[*@#'$\\\]",'',reply['message']))]
     reply_node_insertion_query = buildInsertOrUpdateNodeQuery('Comment',reply['id'],attributeList)
     attributeList = []
     post_comment_relationship_query = buildInsertOrUpdateRelationshipQuery('BELONGS_TO', 'Comment', reply['id'], 'Post', post_id, attributeList)
     comment_reply_relationship_query= buildInsertOrUpdateRelationshipQuery('REPLIES_TO', 'Comment', reply['id'], 'Comment', comment_id, attributeList)
-    attributeList = [('name', reply['from']['name'])]
+    attributeList = [('name', re.sub("[*@#'$]", '',reply['from']['name']))]
     user_node_insertion_query = buildInsertOrUpdateNodeQuery('User', reply['from']['id'], attributeList)
     attributeList = []
     reply_user_relationship_query = buildInsertOrUpdateRelationshipQuery('POSTED', 'User', reply['from']['id'],'Comment',reply['id'],attributeList)
@@ -96,11 +62,11 @@ def getReplyRelatedData(reply, comment_id, post_id):
 
 def getCommentRelatedData(comment, post_id):
     queryList = []
-    attributeList = [('date',comment['created_time']),('text', comment['message'] )]
+    attributeList = [('date',comment['created_time']),('text', re.sub("[*@#'$\\\]",'',comment['message']))]
     comment_node_insertion_query = buildInsertOrUpdateNodeQuery('Comment',comment['id'], attributeList)
     attributeList = []
     post_comment_relationship_query = buildInsertOrUpdateRelationshipQuery('BELONGS_TO', 'Comment', comment['id'], 'Post', post_id, attributeList)
-    attributeList = [('name', comment['from']['name'])]
+    attributeList = [('name', re.sub("[*@#'$]", '',comment['from']['name']))]
     user_node_insertion_query = buildInsertOrUpdateNodeQuery('User', comment['from']['id'], attributeList)
     attributeList = []
     comment_user_relationship_query = buildInsertOrUpdateRelationshipQuery('POSTED', 'User', comment['from']['id'],'Comment',comment['id'],attributeList)
@@ -128,7 +94,7 @@ def getPostRelatedData(post, site_name):
     post_date = post['created_time']
     post_link = post['link']
     post_facebook_link = 'https://www.facebook.com/' + post_id
-    post_title = post['name'] if 'name' in post else 'Sin título'
+    post_title = re.sub("[*@#'$]", '',post['name']) if 'name' in post else 'Sin título'
     post_reaction_count = post['like']['summary']['total_count']  + post['love']['summary']['total_count'] +\
                           post['wow']['summary']['total_count']   + post['haha']['summary']['total_count'] +\
                           post['sad']['summary']['total_count']   + post['angry']['summary']['total_count']
@@ -159,7 +125,7 @@ def getPostRelatedData(post, site_name):
 
 
 
-def addPosts(client_id, client_secret, site_id, site_name, since_date,until_date, version="2.10"):
+def getRangeQueryList(client_id, client_secret, site_id, site_name, since_date,until_date, version="2.10"):
     queryList = []
     fb_token = getAccessToken(client_id, client_secret)
     since = '1506902400'
@@ -173,18 +139,15 @@ def addPosts(client_id, client_secret, site_id, site_name, since_date,until_date
 
     for post in next_item['data']:
         queryList.extend(getPostRelatedData(post, site_name))
+        while 'paging' in next_item and 'next' in next_item['paging']:
+            next_item = url_retry(next_item['paging']['next'])
+            for post in next_item:
+                queryList.extend(getPostRelatedData(post, site_name))
 
-    print(1)
-
-  # #while 'paging' in next_item and 'next' in next_item['paging']:
-  #     next_item = url_retry(next_item['paging']['next'])
-  #     for post in next_item['data']:
-  #         getPostRelatedData(post, site_name)
-
+    return queryList
 
 
 
-
-addPosts("264737207353432","460c5a58dd6ddd6997b2645b1ad37cdd","115872105050", "", "","","2.10")
+#getRangeQueryList("264737207353432","460c5a58dd6ddd6997b2645b1ad37cdd","47921680333", "", "","","2.10")
 
 #115872105050?fields=posts{id,created_time,name}&limit=100
--- test.py
+++ test.py
@@ -1,5 +1,5 @@
-from cvs import scrape_fb,buildCommentsCSVs, buildPostCSVs
-
+from fb_scrape_public import getRangeQueryList
+from neo4j.v1 import GraphDatabase, basic_auth #to connect to the database
 
 #Dictionary of the Medios
 dicMedios = {'Nacion'	:'115872105050',
@@ -14,25 +14,18 @@ AppID = '264737207353432'
 AppSecret = '460c5a58dd6ddd6997b2645b1ad37cdd'
 
 
+#queryList = getRangeQueryList(AppID,AppSecret,dicMedios['Financiero'],'El Financiero','','','2.10')
+queryList = getRangeQueryList("264737207353432","460c5a58dd6ddd6997b2645b1ad37cdd","47921680333", "", "","","2.10")
+#Connection with the database
+driver = GraphDatabase.driver("bolt://localhost:7687", auth=basic_auth("neo4j", "neo4j"))
+session = driver.session()
 
-#Nacion_posts = scrape_fb(AppID, AppSecret, dicMedios['Nacion'], "sample_posts.csv", '2.7', 'posts', '2017-10-02')	# Request post from La Nacion's FB page
+queryNumber = len(queryList)
+queryCounter = 0
+for query in queryList:
+    session.run(query)
+    queryCounter += 1
+    print(queryCounter,' de ',queryNumber,' consultas ejecutadas')
 
+session.close()
 
-#Comments = scrape_fb(AppID, AppSecret, '115872105050_10159475804280051', "sample_comments.csv", '2.7', 'comments', '2017-10-02')
-#comments = scrape_fb('264737207353432','460c5a58dd6ddd6997b2645b1ad37cdd','115872105050_10159350223445051',scrape_mode="comments")
-
-#buildCommentsCSVs(AppID, AppSecret, dicMedios['Nacion'], 'nodos_comments_Nacion.csv', 'aristas_comments_Nacion.csv', version="2.10")
-
-
-buildPostCSVs(AppID, AppSecret, dicMedios['Nacion'], 'nodos_posts_Nacion.csv', 'aristas_posts_Nacion.csv', version="2.10")
-
-
-#buildCommentsCSVs(AppID, AppSecret, dicMedios['CRHoy'], 'nodos_comments_CRHoy.csv', 'aristas_comments_CRHoy.csv', version="2.10")
-
-
-#buildPostCSVs(AppID, AppSecret, dicMedios['CRHoy'], 'nodos_posts_CRHoy.csv', 'aristas_posts_CRHoy.csv', version="2.10")
-
-#KonzulTICAS
-#115872105050/posts?fields=id,created_time,name,comments{id,message,comments{id,message}}&limit=10
-
-#115872105050?fields=posts{id,created_time,name,comments{id,message,comments{id,message}}}&limit=10
